67c29cc9ffbba7cf732319a0fb0b7acfecab2a84
aniket965 <aniket965.as@gmail.com>
2017-10-19 18:35:41 +0530

    Publised npm package🐕🐕

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Emoji-Logger
+# emojifylogs [NPM]('https://www.npmjs.com/package/emojifylogs)
+Convert basic logs into more intresting Log using Emoji logs.
+it Contains different type of loggers you can choose your type or suggest for more Loggers
+## Definations
+```javascript
+
+var log = require('emojifylogs').log
+var Doglogger = require('emojifylogs').Doglogger
+var Catlogger = require('emojifylogs').Catlogger
+```
+
+## Usage
+```javascript
+log('Lol')
+log.info('Awesome info')
+log.error('Awesome Error')
+log.warn('Awesome Warn')
+/* Logs =>
+😍  Lol
+🤓  Awesome info
+😡  Awesome Error
+😒  Awesome War
+*/
+```
--- test.js
+++ test.js
@@ -1,17 +1,8 @@
 var log = require('./app').log
 var Doglogger = require('./app').Doglogger
 var Catlogger = require('./app').Catlogger
-log("hahaha")
-log.info("Awesome")
-log.error("check your grammer")
-log.warn('agli baar s nhi bolunga this lgne k liye')
 
-Catlogger("hahaha")
-Catlogger.info("Awesome")
-Catlogger.error("check your grammer")
-Catlogger.warn('agli baar s nhi bolunga this lgne k liye')
-
-Doglogger("hahaha")
-Doglogger.info("Awesome")
-Doglogger.error("check your grammer")
-Doglogger.warn('agli baar s nhi bolunga this lgne k liye')
+log('Lol')
+log.info('Awesome info')
+log.error('Awesome Error')
+log.warn('Awesome Warn')
